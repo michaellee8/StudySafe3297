75ad6da3b3342a50c0e5c8dfb2c603ddb75430da
michaellee8 <ckmichael8@gmail.com>
2022-05-06 21:26:37 +0800

    chore: wip

--- study-safe-trace/src/App.tsx
+++ study-safe-trace/src/App.tsx
@@ -8,19 +8,20 @@ import { theme } from "./theme";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import IndexPage from "./pages/Index";
 import SignIn from "./pages/SignIn";
+import Trace from "./pages/Trace";
 
 function App() {
-  const [axiosInstance, setAxiosInstance] = useState<AxiosInstance>(
-    getAxiosInstance(),
-  );
   const [authToken, setAuthToken] = useState<string | undefined>(undefined);
+  const [axiosInstance, setAxiosInstance] = useState<AxiosInstance>(() =>
+    getAxiosInstance(authToken),
+  );
   const loginWithToken = (token?: string) => {
     setAuthToken(token);
-    setAxiosInstance(getAxiosInstance(token));
+    setAxiosInstance(() => getAxiosInstance(token));
   };
   const logout = () => {
     setAuthToken(undefined);
-    setAxiosInstance(getAxiosInstance());
+    setAxiosInstance(() => getAxiosInstance());
   };
   return (
     <React.Fragment>
@@ -33,6 +34,7 @@ function App() {
               path="/signin"
               element={<SignIn setToken={loginWithToken} />}
             />
+            <Route path="/trace" element={<Trace />} />
             <Route path="*" element={<IndexPage authToken={authToken} />} />
           </Routes>
         </BrowserRouter>
--- study-safe-trace/src/pages/Trace.tsx
+++ study-safe-trace/src/pages/Trace.tsx
@@ -1,8 +1,59 @@
+import { Button } from "@mui/material";
+import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import React, { useContext } from "react";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import React, { useContext, useState } from "react";
 import { AxiosContext } from "../AxiosContext";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import { LocalizationProvider } from "@mui/x-date-pickers";
 
 export default function Trace() {
   const axiosInstance = useContext(AxiosContext);
-  return <Container></Container>;
+  const [studentId, setStudentId] = useState<string>("");
+  const [startDateTime, setStartDateTime] = useState<Date | null>(null);
+  const [endDateTime, setEndDateTime] = useState<Date | null>(null);
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterMoment}>
+      <Container>
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Sign in to proceed
+          </Typography>
+          <Box sx={{ mt: 1 }} component="form" onSubmit={handleSignIn}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Student UID"
+              type="text"
+              id="password"
+              name="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+            />
+            <Button
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              type="submit"
+            >
+              Sign in
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </LocalizationProvider>
+  );
 }
